c1cb7d33f4abb14f71d19e0a19d4d6d1577fd5c0
Herr Karotte <dvp03ne@yandex.ru>
2023-11-10 14:53:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method3
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>(50);
+            string str = "";
+            Console.Write(": ");
+            str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(') stack.Push(str[i]);
+                else if (str[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("Nope");
+                        Console.WriteLine($"Extra ')' in position{i + 1}");
+                        return 1;
+                    }
+                    stack.Pop();
+                }
+            }
+            if (stack.Count != 0)
+            {
+                Console.WriteLine("Nope");
+                Console.WriteLine($"Extra '(' in quantity: {stack.Count}");
+            }
+            else Console.WriteLine("Yep");
+            return 0;
+        }
+    }
+}
